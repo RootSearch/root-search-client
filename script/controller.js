9e91dde82324d19de0a2a9d6f29d63733fc85b1d
Lumy Kelvin <lunox273@gmail.com>
2020-10-17 02:41:53 +0900

    make coordination map

--- script/controller.js
+++ script/controller.js
@@ -34,15 +34,17 @@ class Controller {
    */
 
   BaseView_ShowOverlay = () => {
-    if ($("body").hasClass("search-mode")) {
-      $(".node-container.root").toggleClass("fall-down default");
-      setTimeout(() => {
-        $(".dynamic-view").toggleClass("search-mode root-mode");
-        $(".node-container.root").toggleClass("fall-down default");
-      }, 2000);
-    } else {
-      $(".dynamic-view").toggleClass("search-mode root-mode");
-    }
+    $(".dynamic-view").toggleClass("search-mode root-mode");
+    $("#search-bar").toggleClass("interaction-view");
+    // if ($("body").hasClass("search-mode")) {
+    //   $(".node-container.root").toggleClass("fall-down default");
+    //   setTimeout(() => {
+    //     $(".dynamic-view").toggleClass("search-mode root-mode");
+    //     $(".node-container.root").toggleClass("fall-down default");
+    //   }, 2000);
+    // } else {
+    //   $(".dynamic-view").toggleClass("search-mode root-mode");
+    // }
 
     // this._model.ChangeViewModel([
     //   { view: "base-view", object: "text-input", data: { show: true } },
