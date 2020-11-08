ccf5ed6d790e1e4c8bd3e0a0c5abae71fc95d7bc
Lumy Kelvin <lunox273@gmail.com>
2020-11-08 23:10:56 +0900

    make viewmodel const

--- script/main.js
+++ script/main.js
@@ -2,52 +2,7 @@ let apigateway;
 $(document).ready(() => {
   console.log("ready");
 
-  let model = new Model({
-    "base-view": {
-      modified: false,
-      object: {},
-    },
-    "dynamic-view": {
-      modified: false,
-      object: {
-        "search-bar": {
-          modified: false,
-          data: {
-            search: "",
-          },
-        },
-        "center-button": {
-          modified: false,
-          data: {
-            mode: "search",
-          },
-        },
-        "dynamic-view-group": {
-          modified: false,
-          data: {
-            mode: "search",
-          },
-        },
-      },
-    },
-    "result-view": {
-      modified: false,
-      object: {
-        results: {
-          modified: false,
-          data: {
-            container: [],
-          },
-        },
-        "result-layer": {
-          modified: false,
-          data: {
-            mode: "search",
-          },
-        },
-      },
-    },
-  });
+  const model = new Model(viewModel);
 
   const view = new View();
   const controller = new Controller();
@@ -56,13 +11,13 @@ $(document).ready(() => {
   const result = new ResultView();
   apigateway = new ApiGateway();
   const nodemap = new NodeMap();
-  const parser = new CodeParser("CultureLand");
+  const parser = new CodeParser();
 
   result.linkObject(view, nodemap);
   dynamic.linkObject(view);
-
   model.linkObject(view);
   controller.linkObject(model, view, apigateway, parser);
+
   view.linkObject(controller, {
     "base-view": base,
     "dynamic-view": dynamic,
@@ -71,3 +26,50 @@ $(document).ready(() => {
 
   controller.addEventHandler();
 });
+
+const viewModel = {
+  "base-view": {
+    modified: false,
+    object: {},
+  },
+  "dynamic-view": {
+    modified: false,
+    object: {
+      "search-bar": {
+        modified: false,
+        data: {
+          search: "",
+        },
+      },
+      "center-button": {
+        modified: false,
+        data: {
+          mode: "search",
+        },
+      },
+      "dynamic-view-group": {
+        modified: false,
+        data: {
+          mode: "search",
+        },
+      },
+    },
+  },
+  "result-view": {
+    modified: false,
+    object: {
+      results: {
+        modified: false,
+        data: {
+          container: [],
+        },
+      },
+      "result-layer": {
+        modified: false,
+        data: {
+          mode: "search",
+        },
+      },
+    },
+  },
+};
