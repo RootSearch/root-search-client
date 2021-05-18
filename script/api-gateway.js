60af7de6576ebeb61901b8870d30dd0b58e96c8e
Lumy Kelvin <lunox273@gmail.com>
2021-05-18 23:18:55 +0900

    add delete api(demo)

--- script/api-gateway.js
+++ script/api-gateway.js
@@ -1,5 +1,6 @@
 class ApiGateway {
-  static __server_path__ = "https://rootsearch.vumigration.com:5001/search";
+  static __server_path__ = "https://rootsearch.vumigration.com:5001";
+  static __search__ = "/search";
 
   linkObject = (controller) => {
     this._controller = controller;
@@ -13,10 +14,11 @@ class ApiGateway {
     this.onPendingHandler = eventHandlers["pending"];
     this.onSuccessHandler = eventHandlers["success"];
     this.onErrorHandler = eventHandlers["error"];
+    this.onDeleteHandler = eventHandlers["delete"];
   };
 
-  startSearch = (targer) => {
-    this.eventSource = this._createEventSource(this.eventSource, targer);
+  startSearch = (target) => {
+    this.eventSource = this._createEventSource(this.eventSource, target);
     this._addEventHandler(this.eventSource, {
       open: this._open,
       pending: this._pending,
@@ -29,6 +31,25 @@ class ApiGateway {
     if (!this.eventSource || this.eventSource.readyState === 2) return;
     this.eventSource.close();
     this.eventSource = undefined;
+  };
+
+  removeKeyword = (target) => {
+    const httpsRequest = new XMLHttpRequest();
+    if (!httpsRequest) {
+      console.log("Fail to Create XMLHTTP Instance!");
+      return;
+    }
+    httpsRequest.onreadystatechange = () => {
+      if (httpsRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpsRequest.status === 200) this._delete();
+        else this._error(httpsRequest.responseText);
+      }
+    };
+    httpsRequest.open(
+      "DELETE",
+      `${ApiGateway.__server_path__}${ApiGateway.__search__}/${target}`
+    );
+    httpsRequest.send();
   };
 
   _open = (e) => {
@@ -47,11 +68,18 @@ class ApiGateway {
     if (this.onErrorHandler) this.onErrorHandler(e);
   };
 
+  _delete = (e) => {
+    if (this.onDeleteHandler) this.onDeleteHandler(e);
+  };
+
   _createEventSource = (eventSource, target) => {
     if (eventSource && eventSource.readyState !== 2) eventSource.close();
-    return new EventSource(`${ApiGateway.__server_path__}/${target}`, {
-      withCredentials: false,
-    });
+    return new EventSource(
+      `${ApiGateway.__server_path__}${ApiGateway.__search__}/${target}`,
+      {
+        withCredentials: false,
+      }
+    );
   };
 
   _addEventHandler = (eventSource, eventHandlers) => {
